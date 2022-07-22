f1abc5c4ba3fa544e8094161b8fb9f68e1a9a485
ShannonTao <shannon.cxq@gmail.com>
2022-07-22 00:59:10 -0400

    added time calculator

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ app.addEventListener = function () {
     app.$passBoolean = $("input[type='radio'][name='passBoolean']:checked");
     app.$playerAmt = $("input[type='radio'][name='playerAmt']:checked");
     app.$hintAmt = $("input[type='radio'][name='hintAmt']:checked");
+    app.$timePlayed = $(".timePlayed").val();
+
     app.readInput();
     // clear radio inputs
   });
@@ -27,24 +29,76 @@ app.readInput = function () {
   const passBoolean = app.$passBoolean.val();
   const playerAmt = app.$playerAmt.val();
   const hintAmt = app.$hintAmt.val();
-  app.addScore(roomName, passBoolean, playerAmt, hintAmt);
+  const timePlayed = app.$timePlayed;
 
-  console.log(roomName, passBoolean, playerAmt, hintAmt);
+  if (
+    roomName == undefined ||
+    passBoolean == undefined ||
+    playerAmt == undefined ||
+    hintAmt == undefined
+  ) {
+    alert("Fill in required fields!");
+  } else {
+    if (timePlayed == "") {
+      const noTime = "N/A";
+      app.addScore(roomName, passBoolean, noTime, playerAmt, hintAmt);
+    } else {
+      const timeDuration = app.calculateTimeRemaining(timePlayed);
+      app.addScore(roomName, passBoolean, timeDuration, playerAmt, hintAmt);
+    }
+  }
 };
 
-app.addScore = function (name, pass, player, hint) {
+app.addScore = function (name, pass, time, player, hint) {
   app.$displayStats.prepend(
     `<ul>
         <li>${name}</li>
         <li>${pass}</li>
+        <li>${time}</li>
         <li>${player}</li>
         <li>${hint}</li>
     </ul>`
   );
 
-  const roomData = { name: name, pass: pass, player: player, hint: hint };
+  const roomData = {
+    name: name,
+    pass: pass,
+    time: time,
+    player: player,
+    hint: hint,
+  };
   $("input[type='radio']").prop("checked", false);
-  roomObj(roomData);
+  $(".timePlayed").val("");
+  //   roomObj(roomData);
+};
+
+// time remaining calculator
+app.calculateTimeRemaining = function (timeRemaining) {
+  let seconds;
+  let minutes;
+  let remainingSeconds;
+  let remainingMinutes;
+  timeRemaining = timeRemaining.replace(":", "");
+
+  if (timeRemaining.length <= 3) {
+    timeRemaining = timeRemaining.padStart(4, "0");
+  }
+
+  const spliceSecond = timeRemaining.slice(-2);
+  const spliceMinute = timeRemaining.slice(0, 2);
+
+  if (spliceSecond == "00") {
+    remainingSeconds = spliceSecond.padStart(2, "0");
+    console.log(remainingSeconds);
+    remainingMinutes = 60 - spliceMinute;
+  } else {
+    remainingSeconds = 60 - spliceSecond.padStart(2, "0");
+    remainingSeconds = remainingSeconds.toString().padStart(2, "0");
+    remainingMinutes = 59 - spliceMinute;
+  }
+
+  const remainingTime = `${remainingMinutes}:${remainingSeconds}`;
+  console.log(remainingTime);
 };
 
 app.init = () => {
